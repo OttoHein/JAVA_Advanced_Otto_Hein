d27d7efefae6a8700f63c266ce45cd90657226b2
OttoHein <33087686+OttoHein@users.noreply.github.com>
2018-08-31 13:29:46 +0200

    Vraag 2 ex 2

--- SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
+++ SPRING-EXAMEN-JAVA2-VRAAG2/src/main/java/edu/ap/spring/Exam.java
@@ -25,12 +25,18 @@ public class Exam {
 	// te tellen in de gegeven string
 	// 1 punt
 	public int countUppercaseCharacters(String sentence) {
+		return (int)sentence.chars()
+					.filter(c -> Character.isUpperCase(c))
+					.count();
 	}
 	
 	// Maak gebruik van lambdas en streams om het aantal keren te tellen
 	// dat string s voorkomt (zowel uppercase als lowercase) in de lijst names
 	// 1 punt
 	public int countChar(List<String> names, String s) {
+		return names.stream()
+					.
+			
 	}
 	
 	// Maak gebruik van lambdas en streams om het hoogste Y coordinaat
